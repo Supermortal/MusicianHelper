6c51f3fa3707af298056f792fdaa2ec9221b2cef
Supermortal <christopherbyronpink@yahoo.com>
2015-09-07 19:48:50 -0600

    Refactored WMVProfile

---
+++ MusicianHelper.Infrastructure/Models/SplicerWMVProfile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicianHelper.Infrastructure.Models.Enums;
+using Splicer.WindowsMedia;
+
+namespace MusicianHelper.Infrastructure.Models
+{
+    public class SplicerWMVProfile
+    {
+        public string Profile { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public static SplicerWMVProfile GetProfile(VideoQuality vq)
+        {
+            var profile = new SplicerWMVProfile();
+
+            switch (vq)
+            {
+                case VideoQuality.FULLHD2_NTSC:
+                    profile.Profile = WindowsMediaProfiles.FULLHD2_NTSC;
+                    profile.Width = 1920;
+                    profile.Height = 1080;
+                    break;
+                case VideoQuality.FullHD:
+                    profile.Profile = WindowsMediaProfiles.FullHD;
+                    profile.Width = 1920;
+                    profile.Height = 1080;
+                    break;
+                case VideoQuality.HD2_NTSC:
+                    profile.Profile = WindowsMediaProfiles.HD2_NTSC;
+                    profile.Width = 1280;
+                    profile.Height = 720;
+                    break;
+                case VideoQuality.HighQuality2_NTSC:
+                    profile.Profile = WindowsMediaProfiles.HighQuality2_NTSC;
+                    profile.Width = 640;
+                    profile.Height = 360;
+                    break;
+                case VideoQuality.HighQuality_NTSC:
+                    profile.Profile = WindowsMediaProfiles.HighQuality_NTSC;
+                    profile.Width = 1440;
+                    profile.Height = 1080;
+                    break;
+                case VideoQuality.HighQuality_PAL:
+                    profile.Profile = WindowsMediaProfiles.HighQuality_PAL;
+                    profile.Width = 1440;
+                    profile.Height = 1080;
+                    break;
+                case VideoQuality.SplicerDefaultHighQualityVideo:
+                    profile.Profile = WindowsMediaProfiles.HighQualityVideo;
+                    profile.Width = 320;
+                    profile.Height = 240;
+                    break;
+                case VideoQuality.SplicerDefaultLowQualityAudio:
+                    profile.Profile = WindowsMediaProfiles.LowQualityAudio;
+                    profile.Width = 0;
+                    profile.Height = 0;
+                    break;
+                case VideoQuality.SplicerDefaultLowQualityVideo:
+                    profile.Profile = WindowsMediaProfiles.LowQualityVideo;
+                    profile.Width = 0;
+                    profile.Height = 0;
+                    break;
+                case VideoQuality.SplicerDefaultMediumQualityAudio:
+                    profile.Profile = WindowsMediaProfiles.MediumQualityAudio;
+                    profile.Width = 0;
+                    profile.Height = 0;
+                    break;
+                case VideoQuality.StandardQuality:
+                    profile.Profile = WindowsMediaProfiles.StandardQuality;
+                    profile.Width = 1280;
+                    profile.Height = 720;
+                    break;
+            }
+
+            return profile;
+        }
+
+    }
+}
--- MusicianHelper.Infrastructure/Services/Concrete/SplicerWMVVideoProcessingService.cs
+++ MusicianHelper.Infrastructure/Services/Concrete/SplicerWMVVideoProcessingService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using log4net;
 using MusicianHelper.Common.Helpers.Log;
+using MusicianHelper.Infrastructure.Models;
 using MusicianHelper.Infrastructure.Models.Enums;
 using MusicianHelper.Infrastructure.Services.Abstract;
 using Splicer.Renderer;
@@ -21,7 +22,7 @@ namespace MusicianHelper.Infrastructure.Services.Concrete
             {
                 using (ITimeline timeline = new DefaultTimeline())
                 {
-                    var profile = GetProfile(vq);
+                    var profile = SplicerWMVProfile.GetProfile(vq);
 
                     var group = timeline.AddVideoGroup(32, profile.Width, profile.Height);
                     var videoTrack = group.AddTrack();
@@ -36,7 +37,6 @@ namespace MusicianHelper.Infrastructure.Services.Concrete
                         videoTrack.AddImage(imagePath, 0, 0, 0, imageDuration);
                     }
 
-                    //IRenderer renderer = new AviFileRenderer(timeline, outputPath);
                     IRenderer renderer = new WindowsMediaRenderer(timeline, outputPath, profile.Profile);
 
                     if (renderCompleted != null)
@@ -53,77 +53,5 @@ namespace MusicianHelper.Infrastructure.Services.Concrete
             }
         }
 
-        private static WMVProfile GetProfile(VideoQuality vq)
-        {
-            var profile = new WMVProfile();
-
-            switch (vq)
-            {
-                case VideoQuality.FULLHD2_NTSC:
-                    profile.Profile = WindowsMediaProfiles.FULLHD2_NTSC;
-                    profile.Width = 1920;
-                    profile.Height = 1080;
-                    break;
-                case VideoQuality.FullHD:
-                    profile.Profile = WindowsMediaProfiles.FullHD;
-                    profile.Width = 1920;
-                    profile.Height = 1080;
-                    break;
-                case VideoQuality.HD2_NTSC:
-                    profile.Profile = WindowsMediaProfiles.HD2_NTSC;
-                    profile.Width = 1280;
-                    profile.Height = 720;
-                    break;
-                case VideoQuality.HighQuality2_NTSC:
-                    profile.Profile = WindowsMediaProfiles.HighQuality2_NTSC;
-                    profile.Width = 640;
-                    profile.Height = 360;
-                    break;
-                case VideoQuality.HighQuality_NTSC:
-                    profile.Profile = WindowsMediaProfiles.HighQuality_NTSC;
-                    profile.Width = 1440;
-                    profile.Height = 1080;
-                    break;
-                case VideoQuality.HighQuality_PAL:
-                    profile.Profile = WindowsMediaProfiles.HighQuality_PAL;
-                    profile.Width = 1440;
-                    profile.Height = 1080;
-                    break;
-                case VideoQuality.SplicerDefaultHighQualityVideo:
-                    profile.Profile = WindowsMediaProfiles.HighQualityVideo;
-                    profile.Width = 320;
-                    profile.Height = 240;
-                    break;
-                case VideoQuality.SplicerDefaultLowQualityAudio:
-                    profile.Profile = WindowsMediaProfiles.LowQualityAudio;
-                    profile.Width = 0;
-                    profile.Height = 0;
-                    break;
-                case VideoQuality.SplicerDefaultLowQualityVideo:
-                    profile.Profile = WindowsMediaProfiles.LowQualityVideo;
-                    profile.Width = 0;
-                    profile.Height = 0;
-                    break;
-                case VideoQuality.SplicerDefaultMediumQualityAudio:
-                    profile.Profile = WindowsMediaProfiles.MediumQualityAudio;
-                    profile.Width = 0;
-                    profile.Height = 0;
-                    break;
-                case VideoQuality.StandardQuality:
-                    profile.Profile = WindowsMediaProfiles.StandardQuality;
-                    profile.Width = 1280;
-                    profile.Height = 720;
-                    break;
-            }
-
-            return profile;
-        }
-    }
-
-    class WMVProfile
-    {
-        public string Profile { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
     }
 }
