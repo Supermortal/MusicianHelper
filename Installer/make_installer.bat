cba928d4b994eefdbb892548d5446b368a18b612
Supermortal <christopherbyronpink@yahoo.com>
2015-09-14 22:29:52 -0600

    Added some errant files

--- Installer/make_installer.bat
+++ Installer/make_installer.bat
@@ -1,30 +1,12 @@
-@echo off
+rem @echo off
 
-SET INSTALLERDIR=%CD%
-cd ..\
-SET BASEDIR=%CD%
-cd %INSTALLERDIR%
-
-rmdir /S /Q InstallerData
-mkdir InstallerData
 cd InstallerData
-SET INSTALLERDATADIR=%CD%
 
-cd ..\..\
+rmdir /S /Q errorlogs
+del MusicianHelper.APIKeyBootstrapper.exe
+del log4net.Debug.config
 
-xcopy *.config %INSTALLERDATADIR% /Q /Y
-xcopy *.json %INSTALLERDATADIR% /Q /Y
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" MusicianHelper.sln /Build "Release"
-cd MusicianHelper.WinForms\bin\Release
-xcopy *.* %INSTALLERDATADIR% /Q /Y
-cd %BASEDIR%\MusicianHelper.APIKeyBootstrapper\bin\Release
-xcopy *.exe %INSTALLERDATADIR% /Q /Y
-
-cd %INSTALLERDATADIR%
-del *.pdb
-start MusicianHelper.APIKeyBootstrapper
-
-cd %INSTALLERDIR%
+cd ..\
 
 rem candle installer.wxs
 rem light installer.wixobj
