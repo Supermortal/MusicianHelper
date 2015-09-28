c6892808d674885020a56de843f1545872a75e04
Supermortal <christopherbyronpink@yahoo.com>
2015-09-27 21:46:16 -0600

    Scaffolded some files

---
+++ MusicianHelper.SinkWriterSandbox/MusicianHelper.SinkWriterSandbox/ImageUtilities.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "ImageUtilities.h"
+
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+ImageUtilities::ImageUtilities()
+{
+}
+
+
+ImageUtilities::~ImageUtilities()
+{
+}
---
+++ MusicianHelper.SinkWriterSandbox/MusicianHelper.SinkWriterSandbox/VideoUtilities.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "VideoUtilities.h"
+
+
+VideoUtilities::VideoUtilities()
+{
+}
+
+
+VideoUtilities::~VideoUtilities()
+{
+}
