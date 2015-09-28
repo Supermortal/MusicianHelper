bd02e05fce566e75443d400ebb58e475cf965b87
Supermortal <christopherbyronpink@yahoo.com>
2015-09-27 21:50:45 -0600

    Added some links to tutorials for image conversion

--- MusicianHelper.SinkWriterSandbox/MusicianHelper.SinkWriterSandbox/ImageUtilities.cpp
+++ MusicianHelper.SinkWriterSandbox/MusicianHelper.SinkWriterSandbox/ImageUtilities.cpp
@@ -1,3 +1,7 @@
+/*http://stackoverflow.com/questions/7305614/include-gdiplus-h-causes-error*/
+/*https://vctipsplusplus.wordpress.com/tag/image-conversion-c/*/
+/*https://msdn.microsoft.com/en-us/library/ms533837(VS.85).aspx*/
+
 #include "stdafx.h"
 #include "ImageUtilities.h"
 
