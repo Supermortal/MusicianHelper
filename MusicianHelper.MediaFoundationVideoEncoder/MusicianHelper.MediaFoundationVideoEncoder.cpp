ecc64b3bd2bbfc3a1c496ad69600d21e67217b43
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-10-23 15:35:06 -0600

    Clean up

--- MusicianHelper.MediaFoundationVideoEncoder/MusicianHelper.MediaFoundationVideoEncoder.cpp
+++ MusicianHelper.MediaFoundationVideoEncoder/MusicianHelper.MediaFoundationVideoEncoder.cpp
@@ -15,7 +15,8 @@ int _tmain(int argc, _TCHAR* argv[])
     vs.videoFps = 30;
     vs.videoEncodingFormat = MFVideoFormat_WMV3;
 
-    VideoEncoder *ve = new VideoEncoder(L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Images\\paper-stained-3-texture.bmp", L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Audio\\sorry_dave.wav", L"output.wmv", vs);
+    //VideoEncoder *ve = new VideoEncoder(L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Images\\paper-stained-3-texture.bmp", L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Audio\\sorry_dave.wav", L"output.wmv", vs);
+    VideoEncoder *ve = new VideoEncoder(L"C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.bmp", L"C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.wav", L"output.wmv", vs);
 
     ve->Encode();
 
