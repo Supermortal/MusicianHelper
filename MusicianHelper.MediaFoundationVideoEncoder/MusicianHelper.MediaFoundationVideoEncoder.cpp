96cde55285de4c61717241426976336623d5a6d5
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-10-23 15:31:12 -0600

    Started work on process wrapper (as opposed to COM)

---
+++ MusicianHelper.MediaFoundationVideoEncoder/MusicianHelper.MediaFoundationVideoEncoder.cpp
@@ -0,0 +1,24 @@
+// MusicianHelper.MediaFoundationVideoEncoder.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "VideoEncoder.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    HRESULT hr = S_OK;
+
+    VideoSettings vs;
+    vs.videoFps = 60;
+    vs.videoBitRate = 800000;
+    vs.videoFps = 30;
+    vs.videoEncodingFormat = MFVideoFormat_WMV3;
+
+    VideoEncoder *ve = new VideoEncoder(L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Images\\paper-stained-3-texture.bmp", L"C:\\Users\\user\\Dropbox\\Cloud\\GitHub\\MusicianHelper\\TEST\\Audio\\sorry_dave.wav", L"output.wmv", vs);
+
+    ve->Encode();
+
+    return SUCCEEDED(hr) ? 0 : 1;
+}
+
