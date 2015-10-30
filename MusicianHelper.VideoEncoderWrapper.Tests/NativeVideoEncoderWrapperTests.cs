79f31e55a1b7092a373b764c6f40a5bd64d2377d
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-10-30 14:08:24 -0600

    Added more tests

--- MusicianHelper.VideoEncoderWrapper.Tests/NativeVideoEncoderWrapperTests.cs
+++ MusicianHelper.VideoEncoderWrapper.Tests/NativeVideoEncoderWrapperTests.cs
@@ -19,8 +19,62 @@ namespace MusicianHelper.VideoEncoderWrapper.Tests
 
             if (File.Exists(outputPath))
                 File.Delete(outputPath);
-           
-            vew.Encode("C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.bmp", "C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.wav", "output.wmv");
+
+            vew.Encode("C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.bmp", "C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.wav", outputPath);
+
+            Assert.True(File.Exists(outputPath));
+
+            var fi = new FileInfo(outputPath);
+            Assert.True(fi.Length > 1000000);
+        }
+
+        [Fact]
+        public void encoder_works_with_mp3_correctly()
+        {
+            var vew = new NativeVideoEncoderProcess.VideoEncoderWrapper();
+            var workingDir = vew.GetWorkingDirectory();
+            var outputPath = Path.Combine(workingDir, "output_mp3.wmv");
+
+            if (File.Exists(outputPath))
+                File.Delete(outputPath);
+
+            vew.Encode("C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.bmp", "C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.mp3", outputPath);
+
+            Assert.True(File.Exists(outputPath));
+
+            var fi = new FileInfo(outputPath);
+            Assert.True(fi.Length > 1000000);
+        }
+
+        [Fact]
+        public void encoder_works_with_jpg_correctly()
+        {
+            var vew = new NativeVideoEncoderProcess.VideoEncoderWrapper();
+            var workingDir = vew.GetWorkingDirectory();
+            var outputPath = Path.Combine(workingDir, "output_jpg.wmv");
+
+            if (File.Exists(outputPath))
+                File.Delete(outputPath);
+
+            vew.Encode("C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.jpg", "C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.wav", outputPath);
+
+            Assert.True(File.Exists(outputPath));
+
+            var fi = new FileInfo(outputPath);
+            Assert.True(fi.Length > 1000000);
+        }
+
+        [Fact]
+        public void encoder_works_with_png_correctly()
+        {
+            var vew = new NativeVideoEncoderProcess.VideoEncoderWrapper();
+            var workingDir = vew.GetWorkingDirectory();
+            var outputPath = Path.Combine(workingDir, "output_png.wmv");
+
+            if (File.Exists(outputPath))
+                File.Delete(outputPath);
+
+            vew.Encode("C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\paper-stained-3-texture.png", "C:\\Users\\chpink\\Home\\sandbox\\MusicianHelper\\TEST\\sorry_dave.wav", outputPath);
 
             Assert.True(File.Exists(outputPath));
 
