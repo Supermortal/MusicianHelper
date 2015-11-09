a40483e2c66b49e70894492cec8100b54c3fa04c
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-11-09 15:15:58 -0700

    Cleanup

--- MusicianHelper.Infrastructure/Services/Concrete/FacebookSocialMediaService.cs
+++ MusicianHelper.Infrastructure/Services/Concrete/FacebookSocialMediaService.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using Facebook;
 using log4net;
 using MusicianHelper.Common.Helpers;
